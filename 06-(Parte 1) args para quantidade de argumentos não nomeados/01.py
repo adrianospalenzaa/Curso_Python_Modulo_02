d12e82775d0374a18f416398dee9699faeb143c0
Adriano Spalenza <108499428+adrianospalenzaa@users.noreply.github.com>
2024-01-24 22:27:42 -0300

    exercício funções

--- 06-(Parte 1) args para quantidade de argumentos não nomeados/01.py
+++ 06-(Parte 1) args para quantidade de argumentos não nomeados/01.py
@@ -17,11 +17,11 @@ def soma(*args):  # empacotamento de argumentos
     return total
 
 
-soma_1_2_3 = soma(1, 2, 3)
-# print(soma_1_2_3)
-
-soma_4_5_6 = soma(4, 5, 6)
-# print(soma_4_5_6)
+# soma_1_2_3 = soma(1, 2, 3)
+# # print(soma_1_2_3)
+#
+# soma_4_5_6 = soma(4, 5, 6)
+# # print(soma_4_5_6)
 
 numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
 outra_soma = soma(*numeros)  # desempacotamento de argumentos chamando a função soma
--- Exercícios/Funções.py
+++ Exercícios/Funções.py
@@ -5,9 +5,27 @@
 # Retorne o total para uma variável e mostre o valor
 # da variável.
 
+def multiplica (*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+
+resultado = multiplica(*numeros)
+print(resultado)
+
 "------------------------------------------------------------"
 
 # Crie uma função que fala se um número é par ou ímpar.
 # Retorne se o número é par ou ímpar.
 
-print("teste")
+def numero (x, y):
+    if x % y == 0:
+        return ('Numero e PAR')
+    else:
+        return ('Nuemro e Impar')
+
+resultado_2 = numero (11,2)
+print(resultado_2)
